6af94f086993579e5f196d79280e77aa7210712c
Paulo Victor Belens <paulovictor55296@gmail.com>
2023-02-16 22:23:59 -0300

    Ajeitando o lint e requisito 21 realizado

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const botaoSubmit = document.querySelector('#submit-btn');
 const botaoValidar = document.querySelector('#validacao');
+const main = document.querySelector('main');
+const formularioPrincipal = document.querySelector('#evaluation-form');
 const validacao = () => {
   const email = document.querySelector('#email').value;
   const senha = document.querySelector('#senha').value;
@@ -39,24 +41,26 @@ const selecionaSubjectsChecked = () => {
   }
   return selectedSubjects;
 };
-console.log(selecionaSubjectsChecked());
-const conteudoFormulario = () => {
-  const formulario = document.querySelector('#evaluation-form');
+
+const conteudoFormulario = (event) => {
+  event.preventDefault();
   const firstName = document.querySelector('#input-name');
   const lastName = document.querySelector('#input-lastname');
   const email = document.querySelector('#input-email');
   const house = document.querySelector('#house');
-  const family = document.querySelector('#input[name="family"]:checked');
-  const evaluation = document.querySelector('#input[name="rate"]:checked');
-
-  formulario.innerHTML = `
-  <p> Nome: ${firstName.value} ${lastName.value} </p>
+  const family = document.querySelector('input[name="family"]:checked');
+  const evaluation = document.querySelector('input[name="rate"]:checked');
+  const formulario = document.createElement('form');
+  formulario.setAttribute('id', 'form-data');
+  formulario.innerHTML = `<p> Nome: ${firstName.value} ${lastName.value} </p>
   <p> Email: ${email.value} </p>
   <p> Casa: ${house.value} </p>
   <p> Família: ${family.value} </p>
   <p> Matérias: ${selecionaSubjectsChecked()} </p>
   <p> Avaliação: ${evaluation.value} </p>
   <p> Observações: ${textarea.value} </p>`;
+  formularioPrincipal.style.display = 'none';
+  main.insertBefore(formulario, main.firstChild);
 };
 
 botaoSubmit.addEventListener('click', conteudoFormulario);
